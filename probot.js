1a49c3b684c205f59ed7fa6e6e33770552d29827
Zeldox55 <39837384+Zeldox55@users.noreply.github.com>
2019-06-15 15:05:57 +0100

    Update probot.js

--- probot.js
+++ probot.js
@@ -1534,46 +1534,7 @@ client.on('message', msg => {
     }
 }
 });
-client.on('message', async message =>{
 
-  if (message.author.omar) return;
-  if (!message.content.startsWith(prefix)) return;
-  if(!message.channel.guild) return message.channel.send('**This Command For Servers Only ! **').then(m => m.delete(5000));
-  if(!message.member.hasPermission('MANAGE_ROLES'));
-  if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply("**I Don't Have `MANAGE_ROLES` Permission**").then(msg => msg.delete(6000))
-  var command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  var args = message.content.split(" ").slice(1);
-    if(command == "mute") {
-      let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-      if(!tomute) return message.reply(":information_source: `#mute @OrochiX` يجب تحديد شخص ") .then(m => m.delete(5000));
-      if(tomute.hasPermission("MANAGE_MESSAGES"))return      message.channel.send('**I Dont Have Permission** `MANAGE_MASSAGEES`');
-      let muterole = message.guild.roles.find(`name`, "Muted");
-  
-      if(!muterole){
-        try{
-          muterole = await message.guild.createRole({
-            name: "Muted",
-            color: "#000000",
-            permissions:[]
-          })
-          message.guild.channels.forEach(async (channel, id) => {
-            await channel.overwritePermissions(muterole, {
-              SEND_MESSAGES: false,
-              ADD_REACTIONS: false
-            });
-          });
-        }catch(e){
-          console.log(e.stack);
-        }
-      }
-
-      await(tomute.addRole(muterole.id));
-      message.channel.send(`**<@${tomute.id}> Has been muted ! :white_check_mark:**`);
-        message.delete();
-
-    }
-  });
   
   const mmss = require('ms');
   client.on('message', async message => {
