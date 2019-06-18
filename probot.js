04f2ff90e7badf51a2226bd3e2f7464d074e7e7d
Zeldox55 <39837384+Zeldox55@users.noreply.github.com>
2019-06-18 23:27:29 +0100

    Update probot.js

--- probot.js
+++ probot.js
@@ -259,7 +259,24 @@ var filter = m => m.author.id === message.author.id;
     });
   })
 })
-  
+    }})
+
+
+const shorten = require('isgd');
+client.on('message', message => {
+ if (message.content.startsWith(prefix + 'short')) {
+    let args = message.content.split(" ").slice(1);
+  if (!args[0]) return message.channel.send('**Usage**: '+ prefix +'short <link>')
+  if (!args[1]) { 
+    shorten.shorten(args[0], function(res) {
+      if (res.startsWith('Error:')) return message.channel.send('**Usage**: '+ prefix +'short <link>');
+      message.channel.send(`The Short Link:**<${res}>**`); 
+    })
+  } else { 
+    shorten.custom(args[0], args[1], function(res) { 
+      if (res.startsWith('Error:')) return message.channel.send(`The Short Link:**${res}**`); 
+      message.channel.send(`The Short Link:**<${res}>**`); 
+ })}}});
   
   
   
