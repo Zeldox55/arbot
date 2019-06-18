99b9e7940ea3a995ce9e292abb9bb666788ef3fe
Zeldox55 <39837384+Zeldox55@users.noreply.github.com>
2019-06-18 23:48:55 +0100

    Update probot.js

--- probot.js
+++ probot.js
@@ -111,74 +111,9 @@ client.on('raw', event => {
 
              
 
-         
-const reply = JSON.parse(fs.readFileSync('./replys.json' , 'utf8'));
-client.on('message', async message => {
-    let messageArray = message.content.split(" ");
-   if(message.content.startsWith(prefix + "setReply")) {
-    let filter = m => m.author.id === message.author.id;
-    let thisMessage;
-    let thisFalse;
+         
 
-    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send('You don\'t have permission').then(msg => {
-       msg.delete(4500);
-       message.delete(4500);
-    });
-    
-    message.channel.send(':pencil: **| من فضلك اكتب الرساله الان... :pencil2: **').then(msg => {
-
-        message.channel.awaitMessages(filter, {
-          max: 1,
-          time: 90000,
-          errors: ['time']
-        })
-        .then(collected => {
-            collected.first().delete();
-            thisMessage = collected.first().content;
-            let boi;
-            msg.edit(':scroll: **| من فضلك اكتب الرد الان... :pencil2: **').then(msg => {
-      
-                message.channel.awaitMessages(filter, {
-                  max: 1,
-                  time: 90000,
-                  errors: ['time']
-                })
-                .then(collected => {
-                    collected.first().delete();
-                    boi = collected.first().content;
-                    msg.edit('✅ **| تم الاعداد بنجاح...  **').then(msg => {
-        
-                      message.channel.awaitMessages(filter, {
-                        max: 1,
-                        time: 90000,
-                        errors: ['time']
-                      })
-                      let embed = new Discord.RichEmbed()
-                      .setTitle('**Done The Autoreply Code Has Been Setup**')
-                      .addField('Message:', `${thisMessage}`)
-                      .addField('Reply:', `${boi}`)
-                      .setThumbnail(message.author.avatarURL)
-                      .setFooter(`${client.user.username}`)
-                     message.channel.sendEmbed(embed)
-    reply[message.guild.id] = {
-        msg: thisMessage,
-        reply: boi,
-    }
-    fs.writeFile("./replys.json", JSON.stringify(reply), (err) => {
-    if (err) console.error(err)
-  })
-   } 
-            )
-        })
-    })
-})
-    })
-}})             
-client.on('message', async message => {
-   if(message.content === reply[message.guild.id].msg) {
-       message.channel.send(reply[message.guild.id].reply)
-   }}
-)
+ 
  
 
  
@@ -235,11 +170,11 @@ var duration = (upgradeTime * 1000)
   if(!isNaN(args[1])) return message.channel.send(':heavy_multiplication_x:| **The Time Be Nambers `` Do the Commend Agin``**');
         let giveEmbed = new Discord.RichEmbed()
       .setAuthor(message.guild.name, message.guild.iconURL)
-      .setDescription(`**${title}** \nReact Whit 🎁 To Enter! \n**Ends  after   ${day} day  ${hou} hour  ${min} minute ${sec} second**`)
+      .setDescription(`**${title}** \nReact Whit 🎉 To Enter! \n**Ends  after   ${day} day  ${hou} hour  ${min} minute ${sec} second**`)
       .setFooter(message.author.username, message.author.avatarURL);
       message.channel.send(' :heavy_check_mark: **Giveaway Created** :heavy_check_mark:' , {embed: giveEmbed}).then(m => {
           message.delete();
-          m.react('🎁');
+          m.react('🎉');
               var giveAwayCut = setInterval(function() {
                   var days        = Math.floor(seconds/24/60/60);
                   var hoursLeft   = Math.floor((seconds) - (days*86400));
@@ -252,20 +187,20 @@ var duration = (upgradeTime * 1000)
                   }
               let updateGiveEmbed = new Discord.RichEmbed()
               .setAuthor(message.guild.name, message.guild.iconURL)
-              .setDescription(`**${title}** \nReact With 🎁 To Enter! \n**Ends  after   ${days} day  ${hours} hour  ${minutes} minute ${remainingSeconds} second**`)
+              .setDescription(`**${title}** \nReact With 🎉 To Enter! \n**Ends  after   ${days} day  ${hours} hour  ${minutes} minute ${remainingSeconds} second**`)
               .setFooter(message.author.username, message.author.avatarURL);
               m.edit(updateGiveEmbed)
             }, 1000);
          setTimeout(() => {
           clearInterval(giveAwayCut)
-           let users = m.reactions.get("🎁").users;
+           let users = m.reactions.get("🎉").users;
            let list = users.array().filter(u => u.id !== client.user.id);
            let gFilter = list[Math.floor(Math.random() * list.length) + 0]
            let endEmbed = new Discord.RichEmbed()
            endEmbed.setAuthor(message.author.username, message.author.avatarURL)
            endEmbed.setTitle(title)
-           endEmbed.addField('Giveaway End !🎁',`Winners : ${gFilter}`)
-         m.edit('** 🎁 GIVEAWAY ENDED 🎁**' , {embed: endEmbed});
+           endEmbed.addField('Giveaway End !🎉',`Winners : ${gFilter}`)
+         m.edit('** 🎉 GIVEAWAY ENDED 🎉**' , {embed: endEmbed});
          },duration);
        });
   }
@@ -278,22 +213,7 @@ var duration = (upgradeTime * 1000)
 
 
 
-const shorten = require('isgd');
-client.on('message', message => {
- if (message.content.startsWith(prefix + 'short')) {
-    let args = message.content.split(" ").slice(1);
-  if (!args[0]) return message.channel.send('**Usage**: '+ prefix +'short <link>')
-  if (!args[1]) { 
-    shorten.shorten(args[0], function(res) {
-      if (res.startsWith('Error:')) return message.channel.send('**Usage**: '+ prefix +'short <link>');
-      message.channel.send(`The Short Link:**<${res}>**`); 
-    })
-  } else { 
-    shorten.custom(args[0], args[1], function(res) { 
-      if (res.startsWith('Error:')) return message.channel.send(`The Short Link:**${res}**`); 
-      message.channel.send(`The Short Link:**<${res}>**`); 
- })}}});
-  
+ 
   
   
 let newsjson = JSON.parse(fs.readFileSync("./news.json", "utf8")) || {}
